187e1e5215a7417f2a38d130024f941587e379da
Siam Talukder <73644547+siamtalukder484@users.noreply.github.com>
2023-01-04 00:39:19 +0600

    validate

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -8,10 +8,14 @@ import CmnButton from '../layouts/CmnButton';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import Authentication from '../authentication/Authentication';
+import Alert from '@mui/material/Alert';
+import {AiFillEye,AiFillEyeInvisible} from "react-icons/ai";
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification} from "firebase/auth";
 // import InputBox from '../heading/InputBox'
 
 
 const Register = () => {
+  const auth = getAuth();
 
   const CommonBtn = styled(Button)({
     width: "395px",
@@ -40,22 +44,59 @@ const Register = () => {
     },
   });
 
+  let [show, setShow] = useState(false);
   let [formData, setFormdata] = useState({
     email: "",
     full_name: "",
     password: "",
     c_password: "",
   });
+
+  
+  let [error, setError] = useState({
+    email: "",
+    full_name: "",
+    password: "",
+    c_password: "",
+  })
+
   let hundleForm = (e) =>{
     let {name,value} = e.target
     setFormdata ({
       ...formData,
       [name]:value
     })
-    console.log(formData)
+    setError({...error,[name]: ""})
   }
+
   let hundleClick = () =>{
-    console.log("ami click")
+    if(formData.email == ""){
+      setError({...error,email: "Email dao"})
+    }
+    else if(formData.full_name == ""){
+      setError({...error,full_name: "Name dao"})
+    }
+    else if(formData.password == ""){
+      setError({...error,password: "password dao"})
+    }
+    else if(formData.c_password == ""){
+      setError({...error,c_password: "Confirm Password dao"})
+    }
+    else{
+      createUserWithEmailAndPassword(auth,formData.email,formData.password).then((user)=>{
+        sendEmailVerification(auth.currentUser)
+          .then(() => {
+            console.log("Email send")
+          });
+      }).catch((error)=>{
+        const errorCode = error.code;
+        if(errorCode.includes("auth/email-already-in-use")){
+          setError({...error,email: "Email already existed"})
+        };
+        
+      })
+    }
+  
   }
   // let hundleForm = (e) =>{
   //   if(e.target.name == "email"){
@@ -84,10 +125,45 @@ const Register = () => {
                   <p className='reg_subtitle'>Free register and you can enjoy it</p>
               </Header>
               <div className='input_main'>
-                <InputBox textChange={hundleForm} name="email" className='reg_email' type='email' label='Email Address' variant='outlined'/>
-                <InputBox textChange={hundleForm} name="full_name" className='reg_name' type='text' label='Full Name' variant='outlined'/>
-                <InputBox textChange={hundleForm} name="password" className='reg_password' type='password' label='Password' variant='outlined'/>
-                <InputBox textChange={hundleForm} name="c_password" className='reg_password' type='password' label='Password' variant='outlined'/>
+                <div className='input_grp'>
+                    <InputBox textChange={hundleForm} name="email" className='reg_email' type='email' label='Email Address' variant='outlined'/>
+                    {error.email &&
+                      <Alert className='error' variant="filled" severity="error">
+                          {error.email}
+                      </Alert>
+                    }
+                </div>
+                <div className='input_grp'>
+                    <InputBox textChange={hundleForm} name="full_name" className='reg_name' type='text' label='Full Name' variant='outlined'/>
+                    {error.full_name &&
+                      <Alert className='error' variant="filled" severity="error">
+                          {error.full_name}
+                      </Alert>
+                    }
+                </div>
+                <div className='input_grp'>
+                    <InputBox textChange={hundleForm} name="password" className='reg_password' type={show ? "text" : "password"} label='Password' variant='outlined'/>
+                    {show 
+                    ?
+                    <AiFillEye onClick={()=>setShow(false)} className='openeye'/>
+                    :
+                    <AiFillEyeInvisible onClick={()=>setShow(true)} className='openeye'/>
+                    }
+                    {error.password &&
+                      <Alert className='error' variant="filled" severity="error">
+                          {error.password}
+                      </Alert>
+                    }
+                </div>
+                <div className='input_grp'>
+                    <InputBox textChange={hundleForm} name="c_password" className='reg_password' type='password' label='Confirm Password' variant='outlined'/>
+                    {error.c_password &&
+                      <Alert className='error' variant="filled" severity="error">
+                          {error.c_password}
+                      </Alert>
+                    }
+                </div>
+                
               </div>
               <div className='reg_btn'>
                 <CmnButton click={hundleClick} bname={CommonBtn} title="sign up"/>
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDagB-OdJ7Jeojd-BxkVe6WTSrgDCqRz4I",
+  authDomain: "chatting-app-726aa.firebaseapp.com",
+  projectId: "chatting-app-726aa",
+  storageBucket: "chatting-app-726aa.appspot.com",
+  messagingSenderId: "434760680491",
+  appId: "1:434760680491:web:3a303fad528f9db238566e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig
